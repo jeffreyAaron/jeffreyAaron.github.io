4146837057706ba5d3e5c703144a12a7e6a0c9e6
Jeffrey Aaron Jeyasingh <jeffreyaaron06@gmail.com>
2021-11-13 15:26:16 -0800

    Add interests reveal feature

---
+++ index.js
@@ -0,0 +1,55 @@
+window.onload = setUpFunctionality;
+
+function setUpFunctionality() {
+    setUpInterestsHoverFunctionality();
+}
+
+
+function setUpInterestsHoverFunctionality() {
+
+    let homeInterestsList = document.getElementById('homeInterestsList');
+    let homeInterestsSection = document.getElementById('homeInterestsSection');
+    // Iterate through the interests lists and obtain each li element
+    homeInterestsList.childNodes.forEach((listElement) => {
+        // each list element has a data attribute called index that aligns the list element to the right homeInterestsInfo
+
+        if (listElement.dataset != undefined) {
+
+            let listIndex = listElement.dataset.index;
+            let correspondingElement = findElementWithCorrespondingDataIndex(listIndex, homeInterestsSection);
+
+            // Set on hover listener
+            listElement.addEventListener("mouseover", () => {
+                correspondingElement.style.opacity = "1";
+                correspondingElement.style.transform = "scale(1.15, 1.15)";
+                // correspondingElement.style.display = "block";
+            })
+            // Set on hover exit listener
+            listElement.addEventListener("mouseout", () => {
+                correspondingElement.style.opacity = "0";
+                correspondingElement.style.transform = "scale(1, 1)";
+                // correspondingElement.style.display = "none";
+            })
+
+        }
+
+    })
+
+}
+
+
+
+// HELPER FUNCTIONS
+
+function findElementWithCorrespondingDataIndex(dataIndex, elementGroupToSearch) {
+    var node = null;
+    elementGroupToSearch.childNodes.forEach((childNode) => {
+        if (childNode.dataset != null) {
+            if (childNode.dataset.index === dataIndex) {
+                node = childNode;
+            }
+        }
+    });
+
+    return node;
+}
