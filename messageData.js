51abae6fb21816ce0d78a7ffa3d6f0f1c2ccdcae
Jeffrey Aaron Jeyasingh <jeffreyaaron06@gmail.com>
2023-09-13 22:17:58 -0700

    Display Message Feature

---
+++ messageData.js
@@ -0,0 +1,28 @@
+
+var msgText = 0
+var msg = 0
+
+var count = 0
+
+window.onload = () => {
+    msgText = document.getElementById("msg")
+    var url = window.location.search
+    var urlParams = new URLSearchParams(url);
+    msg = urlParams.get("msg")
+
+
+    console.log(url)
+    updateView()
+}
+
+
+function updateView() {
+    msgText.textContent = msg.substring(0, count)
+
+    count++
+
+    if(count <= msg.length) {
+        setTimeout(updateView, 500)
+    }
+}
+
